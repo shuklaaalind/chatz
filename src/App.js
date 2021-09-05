7a71b4a19675eda0560d5067c1d923ccb273248e
Aalind Shukla <shuklaaalind@gmail.com>
2021-09-05 15:43:23 +0530

    Basic Package Added.
    Stream-chat, Stream-chat-react, Universal-cookie

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+import ChanelListContainer from "./components/ChanelListContainer";
+import ChanelContainer from "./components/ChanelContainer";
+
+const apiKey = "wqjd846ujssw";
+
+const client = StreamChat.getInstance(apiKey);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app_wrapper">
+      <Chat client={client} theme="team light">
+        <ChanelListContainer />
+        <ChanelContainer />
+      </Chat>
     </div>
   );
 }
---
+++ src/components/ChanelListContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ChanelListContainer() {
+  return <div></div>;
+}
+
+export default ChanelListContainer;
